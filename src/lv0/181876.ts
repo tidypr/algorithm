a060b86a0ebcff1ee08f0cdf694d080176bfe0f7
tidypr <tidypr@icloud.com>
2025-08-22 10:51:37 +0900

    lv0_181876

---
+++ src/lv0/181876.ts
@@ -0,0 +1,8 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181876
+
+function solution(myString: string) {
+  return myString.toLowerCase();
+}
+
+const result = solution("aBcDeFg");
+console.log(result);
