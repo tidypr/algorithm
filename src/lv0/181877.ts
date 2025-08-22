2805a61653c9b0fef26fd323b5276f2c06a88c85
tidypr <tidypr@icloud.com>
2025-08-22 14:12:21 +0900

    lv0_181877

---
+++ src/lv0/181877.ts
@@ -0,0 +1,9 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181877
+
+function solution(myString: string) {
+  return myString.toUpperCase();
+}
+
+const result = solution("aBcDeFg");
+
+console.log(result);
