546df4419464703e608cc7240d44fcf824290a9e
tidypr <tidypr@icloud.com>
2025-09-04 12:54:50 +0900

    lv0_120802_120807

---
+++ src/lv0_12/120802_120807.ts
@@ -0,0 +1,46 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120802
+
+const q120802 = (num1: number, num2: number) => {
+  return num1 + num2;
+};
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120803
+
+function q120803(num1: number, num2: number) {
+  return num1 - num2;
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120804
+
+function q120804(num1: number, num2: number) {
+  return num1 * num2;
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120805
+
+function q120805(num1: number, num2: number) {
+  return Math.floor(num1 / num2);
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120806
+
+function q120806(num1: number, num2: number) {
+  return Math.floor((num1 / num2) * 1000);
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120807
+
+function q120807(num1: number, num2: number) {
+  return num1 === num2 ? 1 : -1;
+}
+
+function solution() {
+  // console.log(q120802(2, 3));
+  // console.log(q120803(2, 3));
+  // console.log(q120804(2, 3));
+  // console.log(q120805(2, 3));
+  // console.log(q120806(2, 3));
+  console.log(q120807(2, 3));
+}
+
+solution();
