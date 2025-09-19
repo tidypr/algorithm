4caff17dd41b57078d43661d5f10f10a91e99a60
tidypr <tidypr@icloud.com>
2025-09-19 09:54:53 +0900

    lv0_120833

---
+++ src/lv0_12/120833.ts
@@ -0,0 +1,5 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120833
+
+function solution(numbers: number[], num1: number, num2: number) {
+    return numbers.slice(num1, num2 + 1);
+}
